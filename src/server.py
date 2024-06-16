221ada411d79b2d3be1cd2738ca32a6db2d7413d
mxtlrr <ruson0914@gmail.com>
2024-06-16 15:18:59 -0500

    Each band has it's own page & add some CSS for band page

--- src/server.py
+++ src/server.py
@@ -4,6 +4,21 @@ import sqlite3
 app = Flask(__name__)
 db_name = 'narchives.db'
 
+# Each band gets it's own page, e.g. /band?id=2
+@app.route('/band')
+def get_band():
+    artist_id = request.args.get('id')
+    if artist_id is None:
+        return "Fuck u want me to do? U didnt provide a band ID!", 400
+    
+    connect = sqlite3.connect(db_name)
+    cursor = connect.cursor()
+    cursor.execute(f'SELECT * FROM artist WHERE ID={artist_id}')
+
+    data = cursor.fetchall()
+    return render_template('band.html', artist=data[0])
+
+
 @app.route('/make_band', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -15,7 +30,7 @@ def index():
         with sqlite3.connect(db_name) as band:
             cursor = band.cursor()
             cursor.execute("INSERT INTO artist \
-                    (NAME,FORMED,SPLITUP,GENRE) VALUES (?,?,?,?,?)",
+                    (NAME,FORMED,SPLITUP,GENRE) VALUES (?,?,?,?)",
                         (bandname, formed, split_up, genre))
             band.commit()
         return render_template("added-band.html")
