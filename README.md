aa711189847cccf7729e572241e18d9c7b434034
mxtlrr <ruson0914@gmail.com>
2024-06-16 14:29:09 -0500

    Base files n SQL schema

---
+++ README.md
@@ -0,0 +1,8 @@
+# Noise-archives
+An Encylopedia Metallum clone for Noisecore and noise-adjacent subgenres.
+
+## Making the DB
+```
+sqlite3 src/narchives.db < src/db.sql
+```
+
---
+++ src/db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE artist (
+  ID          INTEGER       PRIMARY KEY AUTOINCREMENT,
+  NAME        TEXT                           NOT NULL, /* Band/artist name */
+  FORMED      INTEGER                        NOT NULL, /* Year formed in   */
+  SPLITUP     INTEGER,                                 /* (optional) Split up year */
+  GENRE       TEXT                           NOT NULL  /* Genre */
+);
+
+/* Album */
+CREATE TABLE album (
+  ID          INTEGER     PRIMARY KEY AUTOINCREMENT,
+  ARTIST_ID   INTEGER                      NOT NULL,
+  ALBUM_NAME  TEXT                         NOT NULL, /* Album name    */
+  YEAR        INTEGER                      NOT NULL, /* Year released */
+
+  FOREIGN KEY (ARTIST_ID) REFERENCES artist(ARTIST_ID)
+);
